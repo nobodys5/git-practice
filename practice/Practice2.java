3311588c0b85d6ed78b73ae59f2840e37a534457
nobodys5 <155369610+nobodys5@users.noreply.github.com>
2024-01-29 14:49:19 +0900

    Add files via upload

--- practice/Practice2.java
+++ practice/Practice2.java
@@ -6,16 +6,19 @@ public class Practice2 {
 
 	public static void main(String[] args) {
 		
-//	if문 코드 학습
-		boolean health = true;
-		// if 문의 논리형 코드로 boolean을 사용하여 진실이냐 거짓이냐를 사용
-		// 코드를 풀이하자면 만약 건강하고 싶으면 health를 하고 그렇지않다면 헬스를 하지마라 라는 풀이다.
-		if(health) {
-			System.out.println("건강하고 싶으면");
-			
+//	삼항연산자 코드연습
+		
+		// 몸무게가 120이상이면 4급, 아니면 3급을 삼항연산자로 나타내시오
+		int kg = 130;// 반대로 110을 입력하면 120이상이 아니기때문에 3급이 출력된다.
+		String result;
+		
+		
+		// 아래와같이 int 변수에 비교할값을 작성한뒤 ?로 true, false를 물어보고 큰따옴표""안에
+		// , 맞다 아니다를 작성하여 논리값을 나타내는 방식으로 출력시 결과값이 출력된다.
+		result = kg > 120 ? "4급" : "3급";
+		
+		System.out.println(result);
+		
 		}
-		if(!health) {
-			System.out.println("그렇지 않다면");
-		}
-	}
+	
 }
---
+++ practice/pr/Practice01.java
@@ -0,0 +1,12 @@
+package practice.pr;
+
+public class Practice01 {
+
+	public static void main(String[] args) {
+
+
+		
+		
+	}
+
+}
