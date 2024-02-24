60a3c5d73defbfef6d1f04a6ce0df4e19994cf8a
nobodys5 <155369610+nobodys5@users.noreply.github.com>
2024-02-25 00:58:39 +0900

    Delete web2-youngpil-20240220 directory

--- web2-youngpil-20240220/src/main/java/com/springboot/web2youngpil20240220/service/board/BoardService.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.web2youngpil20240220.service.board;
-
-import com.springboot.web2youngpil20240220.web.dto.board.CreateBoardReqDto;
-
-public interface BoardService {
-
-	public boolean createBoard(CreateBoardReqDto boardReqDto) throws Exception;
-	
-}
--- web2-youngpil-20240220/src/main/java/com/springboot/web2youngpil20240220/web/controller/api/di/TestInterface.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.web2youngpil20240220.web.controller.api.di;
-
-public interface TestInterface {
-
-	public void a();
-	public void b();
-}
